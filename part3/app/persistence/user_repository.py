a283422f822407bdde81178e57080c6cb75d78ed
aureliopagan <aurepgst@gmail.com>
2025-07-29 22:38:59 -0400

    Updating the map entities and relationships

---
+++ part3/app/persistence/user_repository.py
@@ -0,0 +1,10 @@
+from app.persistence.repository import SQLAlchemyRepository
+
+
+class UserRepository(SQLAlchemyRepository):
+    def __init__(self, model):
+        super().__init__(model)
+
+
+    def get_user_by_email(self, email):
+        return self.model.query.filter_by(email=email).first()
